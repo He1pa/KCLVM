6234f91c678e500276172d84a9fb9456950c5230
He1pa <18012015693@163.com>
2022-07-28 15:33:17 +0800

    record used module

--- kclvm/sema/src/resolver/var.rs
+++ kclvm/sema/src/resolver/var.rs
@@ -84,6 +84,11 @@ impl<'ctx> Resolver<'ctx> {
                 }
             }
         } else {
+            if !pkgpath.is_empty() {
+                if let Some(module_scope) = self.scope.borrow_mut().elems.get_mut(pkgpath) {
+                    module_scope.borrow_mut().used = true;
+                }
+            }
             // Load type
             let mut ty = self.resolve_var(
                 &[if !pkgpath.is_empty() {
