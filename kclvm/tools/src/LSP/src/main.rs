d977bfa1680baefa9440f665b959b2f86efc44c2
He1pa <18012015693@163.com>
2023-02-20 13:59:26 +0800

    Feat(LSP): KCL LSP server semantic token provider  (#297)

--- kclvm/tools/src/LSP/src/main.rs
+++ kclvm/tools/src/LSP/src/main.rs
@@ -2,6 +2,9 @@ use chrono::{Local, TimeZone};
 use indexmap::IndexSet;
 use kclvm_tools::lint::lint_files;
 use kclvm_tools::util::lsp::kcl_diag_to_lsp_diags;
+use kclvm_parser::load_program;
+use kclvm_sema::resolver::resolve_program;
+use kclvm_tools::util::lsp::{LEGEND_TYPE, get_scope_imcomplete_sema_token};
 use tower_lsp::jsonrpc::Result;
 use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
@@ -26,6 +29,31 @@ impl LanguageServer for Backend {
                 text_document_sync: Some(TextDocumentSyncCapability::Kind(
                     TextDocumentSyncKind::FULL,
                 )),
+                semantic_tokens_provider: Some(
+                    SemanticTokensServerCapabilities::SemanticTokensRegistrationOptions(
+                        SemanticTokensRegistrationOptions {
+                            text_document_registration_options: {
+                                TextDocumentRegistrationOptions {
+                                    document_selector: Some(vec![DocumentFilter {
+                                        language: Some("KCL".to_string()),
+                                        scheme: Some("file".to_string()),
+                                        pattern: None,
+                                    }]),
+                                }
+                            },
+                            semantic_tokens_options: SemanticTokensOptions {
+                                work_done_progress_options: WorkDoneProgressOptions::default(),
+                                legend: SemanticTokensLegend {
+                                    token_types: LEGEND_TYPE.clone().into(),
+                                    token_modifiers: vec![],
+                                },
+                                range: Some(false),
+                                full: Some(SemanticTokensFullOptions::Bool(true)),
+                            },
+                            static_registration_options: StaticRegistrationOptions::default(),
+                        },
+                    ),
+                ),
                 ..ServerCapabilities::default()
             },
         })
@@ -76,6 +104,74 @@ impl LanguageServer for Backend {
         self.client
             .log_message(MessageType::INFO, "file closed!")
             .await;
+    }
+    async fn semantic_tokens_full(
+        &self,
+        params: SemanticTokensParams,
+    ) -> Result<Option<SemanticTokensResult>> {
+        let filename = params.text_document.uri.path();
+        self.client
+            .log_message(
+                MessageType::INFO,
+                format!("semantic_token_full filename:{}", filename),
+            )
+            .await;
+
+        let mut program = load_program(&[filename], None).unwrap();
+        let prog_scope = resolve_program(&mut program);
+        
+        let mut imcompletesemantictokens = vec![];
+        let scope_map = prog_scope.scope_map.clone();
+        for (_, scope) in scope_map.iter() {
+            let s = scope.borrow();
+            imcompletesemantictokens.append(&mut get_scope_imcomplete_sema_token(&s, filename));
+        }
+
+
+        imcompletesemantictokens.sort_by(|a, b| {
+            if a.start.line == b.start.line {
+                a.start
+                    .column
+                    .unwrap_or(0)
+                    .cmp(&b.start.column.unwrap_or(0))
+            } else {
+                a.start.line.cmp(&b.start.line)
+            }
+        });
+
+        let mut pre_line = 0;
+        let mut pre_start = 0;
+
+        let semantic_tokens: Vec<SemanticToken> = imcompletesemantictokens
+            .iter()
+            .map(|obj| {
+                let line = obj.start.line - 1;
+                let start = obj.start.column.unwrap_or(0);
+
+                let delta_line: u32 = (line - pre_line) as u32;
+                let delta_start: u32 = (if delta_line == 0 {
+                    start - pre_start
+                } else {
+                    start
+                }) as u32;
+                let length = obj.length;
+                let ret = SemanticToken {
+                    delta_line,
+                    delta_start,
+                    length,
+                    token_type: obj.kind,
+                    token_modifiers_bitset: 0,
+                };
+                pre_line = line;
+                pre_start = start;
+                ret
+            })
+            .collect();
+
+        return Ok(Some(SemanticTokensResult::Tokens(SemanticTokens {
+            result_id: None,
+            data: semantic_tokens,
+        })));
     }
 }
 
--- kclvm/tools/src/util/lsp.rs
+++ kclvm/tools/src/util/lsp.rs
@@ -4,6 +4,32 @@ use kclvm_error::Message;
 use kclvm_error::Position as KCLPos;
 use tower_lsp::lsp_types::*;
 
+use kclvm_sema::resolver::scope::Scope;
+use kclvm_sema::resolver::scope::ScopeObject;
+
+#[derive(Debug)]
+pub struct ImCompleteSemanticToken {
+    pub start: KCLPos,
+    /// The scope object end position.
+    pub end: KCLPos,
+    /// The scope object kind.
+    pub kind: u32,
+    pub length: u32,
+}
+
+pub const LEGEND_TYPE: &[SemanticTokenType] = &[
+    SemanticTokenType::FUNCTION,
+    SemanticTokenType::VARIABLE,
+    SemanticTokenType::STRING,
+    SemanticTokenType::STRUCT,
+    SemanticTokenType::COMMENT,
+    SemanticTokenType::NUMBER,
+    SemanticTokenType::KEYWORD,
+    SemanticTokenType::OPERATOR,
+    SemanticTokenType::PARAMETER,
+    SemanticTokenType::PROPERTY,
+];
+
 /// Convert pos format
 /// The position in lsp protocol is different with position in ast node whose line number is 1 based.
 pub fn kcl_pos_to_lsp_pos(pos: &KCLPos) -> Position {
@@ -47,4 +73,69 @@ pub fn kcl_diag_to_lsp_diags(diag: &KCLDiagnostic, file_name: &str) -> Vec<Diagn
         .filter(|msg| msg.pos.filename == file_name)
         .map(|msg| kcl_msg_to_lsp_diags(msg, kcl_err_level_to_severity(diag.level)))
         .collect()
+}
+
+pub fn kcl_scopeobj_to_lsp_imcomplete_sema_token(
+    obj: &ScopeObject,
+) -> Option<ImCompleteSemanticToken> {
+    match obj.kind {
+        kclvm_sema::resolver::scope::ScopeObjectKind::Variable => Some(ImCompleteSemanticToken {
+            start: obj.start.clone(),
+            end: obj.end.clone(),
+            kind: LEGEND_TYPE
+                .iter()
+                .position(|item| item == &SemanticTokenType::VARIABLE)
+                .unwrap() as u32,
+            length: obj.name.len() as u32,
+        }),
+        kclvm_sema::resolver::scope::ScopeObjectKind::Attribute => Some(ImCompleteSemanticToken {
+            start: obj.start.clone(),
+            end: obj.end.clone(),
+            kind: LEGEND_TYPE
+                .iter()
+                .position(|item| item == &SemanticTokenType::PROPERTY)
+                .unwrap() as u32,
+            length: obj.name.len() as u32,
+        }),
+        kclvm_sema::resolver::scope::ScopeObjectKind::Definition => {
+            let mut start_pos = obj.start.clone();
+            start_pos.column =  Some(start_pos.column.unwrap_or(0) + "schema ".len() as u64  );
+            Some(ImCompleteSemanticToken {
+            start: start_pos,
+            end: obj.end.clone(),
+            kind: LEGEND_TYPE
+                .iter()
+                .position(|item| item == &SemanticTokenType::STRUCT)
+                .unwrap() as u32,
+            length: obj.name.len() as u32,
+        })},
+        kclvm_sema::resolver::scope::ScopeObjectKind::Parameter => Some(ImCompleteSemanticToken {
+            start: obj.start.clone(),
+            end: obj.end.clone(),
+            kind: LEGEND_TYPE
+                .iter()
+                .position(|item| item == &SemanticTokenType::PARAMETER)
+                .unwrap() as u32,
+            length: obj.name.len() as u32,
+        }),
+        kclvm_sema::resolver::scope::ScopeObjectKind::TypeAlias => None,
+        kclvm_sema::resolver::scope::ScopeObjectKind::Module => None,
+    }
+}
+
+pub fn get_scope_imcomplete_sema_token(scope: &Scope, file_name: &str) -> Vec<ImCompleteSemanticToken>{
+    let mut tokens = vec![];
+    for (_, obj) in &scope.elems {
+        let obj = obj.borrow();
+        if obj.start.filename == file_name{
+            if let Some(token) = kcl_scopeobj_to_lsp_imcomplete_sema_token(&obj){
+                tokens.push(token);
+            }
+        }
+    }
+    for child in &scope.children{
+        let child = child.borrow();
+        tokens.append(&mut get_scope_imcomplete_sema_token(&child, file_name));
+    }
+    tokens
 }
